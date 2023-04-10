5432b02d55afe2199d07e9a1b609ffa3d8e31900
Aisa Selvira <aisaselvira2020@gmail.com>
2023-04-10 14:01:48 +0700

    UTS

--- No2/KTP.java
+++ No2/KTP.java
@@ -1,81 +1,29 @@
-package No2;
-
-import No1.Subject;
-
 public class KTP {
-    private double[] no_telp ;
-    private String[] tgl_lahir;
-    private String name;
+    private String nama, telefon;
+    private String[] tgl_lhr;
 
-    public KTP(String name) {
-        this.name = name;
+    public KTP(String nama) {
+        this.nama = nama;
+        this.telefon = "088888888888";
+        this.tgl_lhr = new String[]{"31", "12", "2012"};
     }
 
-    public void setNo_telp(double no_telp){
-        no_telp = new double[12];
-        this.no_telp = no_telp;
+    public void setTelefon(String telefon) {
+        if (TelefonValid(telefon)) {
+            this.telefon = telefon;
+            System.out.println("Pergantian No Telepon Berhasil");
+        } else {
+            System.out.println("Nomor Telepon tidak valid");
+        }
+    }
+
+    public void setTgl_lhr(String[] tgl_lhr) {
+        if (TglLhrValid(tgl_lhr)) {
+            this.tgl_lhr = tgl_lhr;
+            System.out.println("Pergantian Tanggal Lahir Berhasil!");
+        } else {
+            System.out.println("Tanggal Lahir tidak valid");
+        }
     }
 
 
-
-    public void setTanggal_lahir(String tanggal_lahir) {
-        this.tanggal_lahir = tanggal_lahir;
-    }
-
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-//public class Website{
-//    private int index = 0;
-//    private String[] usernames;
-//    private String[] passwords;
-//
-//    public Website() {
-//        this.usernames = new String[99];
-//        this.passwords = new String[99];
-//    }
-//
-//    public void addAccount(Account A){
-//        Boolean istrue = true;
-//        for(int i = 0; i<index; i++) {
-//            if (A.getUsername() == usernames[i]){
-//                istrue = false;
-//            }
-//        }
-//        if(istrue == true){
-//            usernames[index] = A.getUsername();
-//            passwords[index] = A.getPassword();
-//            index+=1;
-//            System.out.println("akun berhasil di tambahkan");
-//        }else{
-//            System.out.println("mohon maaf username telah di gunakan");
-//        }
-//    }
-//
-//    public void login(String username, String password){
-//        for(int i = 0; i<index; i++) {
-//            if (username == usernames[i] && password == passwords[i]) {
-//                System.out.println("Login berhasil");
-//            }else{
-//                System.out.println("akun tidak di temukan");
-//            }
-//        }
-//    }
-////        public void encrypt(String n){
-////        String tmp
-////            System.out.println();
-//
-//}
